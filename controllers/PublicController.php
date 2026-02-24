2815ddbe39eb28079b2cca61b57dc6c4bbc794e4
ananthsridev <ananth@dhanlap.com>
2026-02-24 16:08:11 +0530

    Redirect public routes to static HTML pages

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -5,31 +5,37 @@ final class PublicController extends BaseController
 {
     public function home(): void
     {
-        $this->render('public/home', ['title' => 'Home']);
+        $this->redirect('/public/index.html');
     }
 
     public function about(): void
     {
-        $this->render('public/about', ['title' => 'About']);
+        $this->redirect('/public/about.html');
     }
 
     public function howItWorks(): void
     {
-        $this->render('public/how_it_works', ['title' => 'How It Works']);
+        $this->redirect('/public/how-it-works.html');
     }
 
     public function features(): void
     {
-        $this->render('public/features', ['title' => 'Features']);
+        $this->redirect('/public/features.html');
     }
 
     public function tamilRelationshipSystem(): void
     {
-        $this->render('public/tamil_relationship_system', ['title' => 'Tamil Relationship System']);
+        $this->redirect('/public/tamil-relationship-system.html');
     }
 
     public function contact(): void
     {
-        $this->render('public/contact', ['title' => 'Contact']);
+        $this->redirect('/public/contact.html');
+    }
+
+    private function redirect(string $path): void
+    {
+        header('Location: ' . $path);
+        exit;
     }
 }
